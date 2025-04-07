a0cbba228d6b4d885ecde57704c6992e37db35bd
Anand-1912 <34396688+Anand-1912@users.noreply.github.com>
2025-04-07 11:40:36 +0100

    Update Add functionality and Enable Multiplication

--- Calculator.cs
+++ Calculator.cs
@@ -1,6 +1,11 @@
 public static class Calculator
 {
-   public static void Add(){}
+   public static void Add()
+   {
+	   // making changes in Add method
+   }
 
-  public static void Sub(){}
+   public static void Sub(){}
+   
+   public static void Multi(){}
 }
